9ac9f6667fe9063d3aac0aa8188f2c224c1c0b68
Emmanuel <emmancipationera@gmail.com>
2025-02-05 12:42:13 +0100

    fix: fix a build error on usesearchparam to use a Suspense

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,11 +2,15 @@
 import { Button } from "@heroui/button";
 import { cn, Form, Input, Spinner } from "@heroui/react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import React from "react";
 import AlertComponent from "@/app/components/places/AlertComponent";
 import { authenticateUser, loginUserApi } from "@/app/services/userServices";
-import { useRouter, useSearchParams } from "next/navigation";
+import {
+  ReadonlyURLSearchParams,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 import { Icon } from "@iconify/react";
 
 export default function Login() {
@@ -18,20 +22,22 @@ export default function Login() {
     responseMessage: "",
   });
   const navigate = useRouter();
-  const param = useSearchParams();
-  const token = param.get("token");
-  const tokenFaceBook = param.get("tokenFacebook");
-
   const toggleNotification = () => {
     setIsOpenRes(!isOpenRes);
   };
 
-  useEffect(() => {
-    // if (!token || !tokenFaceBook) return;
-    if (token) authenticateUser(token);
-    if (tokenFaceBook) authenticateUser(tokenFaceBook);
-    if (token || tokenFaceBook) navigate.push("/");
-  }, [token]);
+  function SetParam() {
+    const param = useSearchParams();
+    const token = param?.get("token");
+    const tokenFaceBook = param?.get("tokenFacebook");
+    useEffect(() => {
+      // if (!token || !tokenFaceBook) return;
+      if (token) authenticateUser(token);
+      if (tokenFaceBook) authenticateUser(tokenFaceBook);
+      if (token || tokenFaceBook) navigate.push("/");
+    }, [token]);
+    return <></>;
+  }
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -84,101 +90,108 @@ export default function Login() {
     }
   };
   return (
-    <div className=" font-[family-name:var(--font-geist-sans)] px-4 sm:px-6">
-      <div className=" my-[5rem] mx-auto max-w-[30rem]">
-        <h2 className=" my-4 text-xl font-semibold">Login to your account</h2>
-        <Form className="w-full " validationErrors={errors} onSubmit={onSubmit}>
-          <Input
-            label="Email"
-            labelPlacement="outside"
-            name="email"
-            placeholder="Enter email address"
-            type="email"
-            className=" w-full my-2"
-          />
-          <Input
-            label="Password"
-            labelPlacement="outside"
-            name="password"
-            placeholder="Enter password"
-            type="password"
-            className=" w-full my-2"
-          />
-
-          <Button
-            type="submit"
-            variant="flat"
-            className="w-full bg-[#2A254B] text-white my-4"
-            disabled={loading}
+    <Suspense>
+      <SetParam />
+      <div className=" font-[family-name:var(--font-geist-sans)] px-4 sm:px-6">
+        <div className=" my-[5rem] mx-auto max-w-[30rem]">
+          <h2 className=" my-4 text-xl font-semibold">Login to your account</h2>
+          <Form
+            className="w-full "
+            validationErrors={errors}
+            onSubmit={onSubmit}
           >
-            {loading ? <Spinner /> : "Submit"}
-          </Button>
+            <Input
+              label="Email"
+              labelPlacement="outside"
+              name="email"
+              placeholder="Enter email address"
+              type="email"
+              className=" w-full my-2"
+            />
+            <Input
+              label="Password"
+              labelPlacement="outside"
+              name="password"
+              placeholder="Enter password"
+              type="password"
+              className=" w-full my-2"
+            />
 
-          <Button
-            type="button"
-            variant="flat"
-            className="w-full bg-[#2A254B] text-white my-2"
-            disabled={loading}
-            onPress={() => {
-              window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google`;
-            }}
-          >
-            {loading ? (
-              <Spinner />
-            ) : (
-              <>
-                {" "}
-                <Icon
-                  className={cn("text-default-900/50 text-white")}
-                  icon="mynaui:brand-google"
-                  width={24}
-                />
-                Login with Google
-              </>
-            )}
-          </Button>
+            <Button
+              type="submit"
+              variant="flat"
+              className="w-full bg-[#2A254B] text-white my-4"
+              disabled={loading}
+            >
+              {loading ? <Spinner /> : "Submit"}
+            </Button>
 
-          <Button
-            type="button"
-            variant="flat"
-            className="w-full bg-[#2A254B] text-white my-2"
-            disabled={loading}
-            onPress={() => {
-              window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/facebook`;
-            }}
-          >
-            {loading ? (
-              <Spinner />
-            ) : (
-              <>
-                {" "}
-                <Icon
-                  className={cn("text-default-900/50 text-white")}
-                  icon="mynaui:brand-facebook"
-                  width={24}
-                />
-                Login with Facebook
-              </>
-            )}
-          </Button>
-        </Form>
+            <Button
+              type="button"
+              variant="flat"
+              className="w-full bg-[#2A254B] text-white my-2"
+              disabled={loading}
+              onPress={() => {
+                window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google`;
+              }}
+            >
+              {loading ? (
+                <Spinner />
+              ) : (
+                <>
+                  {" "}
+                  <Icon
+                    className={cn("text-default-900/50 text-white")}
+                    icon="mynaui:brand-google"
+                    width={24}
+                  />
+                  Login with Google
+                </>
+              )}
+            </Button>
 
-        <div className="flex justify-between items-center">
-          <div>
-            <Link href="/auth/register"> Sign up</Link>
-          </div>
-          <div>
-            <Link href="/auth/reset-password"> Reset password</Link>
+            <Button
+              type="button"
+              variant="flat"
+              className="w-full bg-[#2A254B] text-white my-2"
+              disabled={loading}
+              onPress={() => {
+                window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/facebook`;
+              }}
+            >
+              {loading ? (
+                <Spinner />
+              ) : (
+                <>
+                  {" "}
+                  <Icon
+                    className={cn("text-default-900/50 text-white")}
+                    icon="mynaui:brand-facebook"
+                    width={24}
+                  />
+                  Login with Facebook
+                </>
+              )}
+            </Button>
+          </Form>
+
+          <div className="flex justify-between items-center">
+            <div>
+              <Link href="/auth/register"> Sign up</Link>
+            </div>
+            <div>
+              <Link href="/auth/reset-password"> Reset password</Link>
+            </div>
           </div>
         </div>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+        <AlertComponent
+          isOpenRes={isOpenRes}
+          toggleNotification={toggleNotification}
+          responseType={response.responseType}
+          responseMessage={response.responseMessage}
+        />
       </div>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
-      <AlertComponent
-        isOpenRes={isOpenRes}
-        toggleNotification={toggleNotification}
-        responseType={response.responseType}
-        responseMessage={response.responseMessage}
-      />
-    </div>
+    </Suspense>
   );
 }
